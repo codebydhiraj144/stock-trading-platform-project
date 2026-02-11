198a1d79492f4e7e4c7bd91c5ae2d2d67a7e7602
Dhiraj kumar Gupta <dhirajgupta49533@gmail.com>
2026-02-11 20:08:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -251,11 +251,8 @@ app.post("/newOrder", async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3002; 
-
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log("✅ MongoDB Connected");
     restoreAllData();
-    // Change '3002' to 'PORT' and add '0.0.0.0'
-    app.listen(PORT, "0.0.0.0", () => console.log(`🚀 Server is Live on Port ${PORT}`));
+    app.listen(3002, () => console.log("🚀 Port 3002 is Live"));
 }).catch(err => console.error("❌ Connection Error:", err));
